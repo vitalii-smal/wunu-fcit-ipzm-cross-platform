8a85c346e5f332e4dce147741e82293a02a3d865
Ninel Pomazova <ninel.pomazova@remt.app>
2023-03-30 16:45:08 +0300

    Remove Sln yask 1

--- IPZm/IPZm/IPZm/ViewModels/StudentsListPageViewModel.cs
+++ IPZm/IPZm/IPZm/ViewModels/StudentsListPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows.Input;
 using IPZm.Models;
 using IPZm.Students.Base;
 using IPZm.Students.BorysHlynskyi;
+using IPZm.Students.NinelPomazova;
 using IPZm.Students.VitaliiSmal;
 using Xamarin.Forms;
 
@@ -15,6 +16,7 @@ namespace IPZm.ViewModels
         {
             { new VitaliiSmalView(), new Student("Vitalii Smal") },
             { new BorysHlynskyiView(), new Student("Borys Hlynskyi") },
+            { new NinelPomazovaView(), new Student("Ninel Pomazova") },
         };
 
         public StudentsListPageViewModel()
