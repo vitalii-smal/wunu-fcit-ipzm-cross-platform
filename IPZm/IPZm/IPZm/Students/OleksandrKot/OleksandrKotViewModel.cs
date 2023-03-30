dc6c05db19cb2b6185673d6662ebeaa9f9bc2007
Oleksandr Kot <kotmodern@gmail.com>
2023-03-30 16:23:19 +0300

    Oleksandr Kot: Practical Task 1

---
+++ IPZm/IPZm/IPZm/Students/OleksandrKot/OleksandrKotViewModel.cs
@@ -0,0 +1,11 @@
+﻿using IPZm.Students.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPZm.Students.OleksandrKot
+{
+    public class OleksandrKotViewModel : BaseStudentViewModel
+    {
+    }
+}
--- IPZm/IPZm/IPZm/ViewModels/StudentsListPageViewModel.cs
+++ IPZm/IPZm/IPZm/ViewModels/StudentsListPageViewModel.cs
@@ -5,6 +5,7 @@ using System.Windows.Input;
 using IPZm.Models;
 using IPZm.Students.Base;
 using IPZm.Students.BorysHlynskyi;
+using IPZm.Students.OleksandrKot;
 using IPZm.Students.VitaliiSmal;
 using Xamarin.Forms;
 
@@ -43,6 +44,7 @@ namespace IPZm.ViewModels
         {
             { new VitaliiSmalView(), vitaliiSmal },
             { new BorysHlynskyiView(), new Student("Borys Hlynskyi") },
+            {new OleksandrKotView(), new Student("Oleksandr Kot") }
         };
 
         public StudentsListPageViewModel()
