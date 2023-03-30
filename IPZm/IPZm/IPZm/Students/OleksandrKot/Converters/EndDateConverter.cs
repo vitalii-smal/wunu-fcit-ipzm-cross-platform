1e57b53d95cc31097878cec2cb2be992ab74164d
Oleksandr Kot <kotmodern@gmail.com>
2023-03-30 16:26:14 +0300

    Oleksandr Kot: Practical Task 2

---
+++ IPZm/IPZm/IPZm/Students/OleksandrKot/Converters/EndDateConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace IPZm.Students.OleksandrKot.Converters
+{
+    public class EndDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var endDate = value as DateTime?;
+            if (endDate == null)
+            {
+                return null;
+            }
+
+            if (endDate.Value == DateTime.Today)
+            {
+                return "current";
+            }
+
+            return endDate.Value.ToString("MMM yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
