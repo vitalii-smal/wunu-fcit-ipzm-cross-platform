020ebb725df5fe5b1f57c74e760dc139ea6bac30
Oleksandr Kot <42963194+LucenTTT@users.noreply.github.com>
2023-04-04 17:49:19 +0300

    Oleksandr Kot: Practical Task 1&2 (without Vitalii changes and .sln changes) (#9)
    * Oleksandr Kot: Practical Task 1
    
    * Revert "Oleksandr Kot: Practical Task 1"
    
    This reverts commit 0bc91c5db63b3e2a1ceeb7fb3da3a22d556d732c.
    
    * Oleksandr Kot: Practical Task 1
    
    * Oleksandr Kot: Practical Task 2
    
    ---------
    
    Co-authored-by: Yaroslav <yaroslav.tsapiv@altoros.com>

---
+++ IPZm/IPZm/IPZm/Students/OleksandrKot/OleksandrKotView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using IPZm.Students.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IPZm.Students.OleksandrKot
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OleksandrKotView : BaseStudentContentPage
+    {
+        private OleksandrKotViewModel _viewModel;
+        public OleksandrKotView()
+        {
+            _viewModel = new OleksandrKotViewModel();
+            BindingContext = _viewModel;
+            InitializeComponent();
+        }
+    }
+}
